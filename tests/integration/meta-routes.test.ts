04783870f8b1c586ca9c1474bafd808f1b492387
harrymees <harry.brundage+harrymees@gmail.com>
2026-04-25 21:00:29 +0000

    test(meta): include discount bulk state in empty snapshot

--- tests/integration/meta-routes.test.ts
+++ tests/integration/meta-routes.test.ts
@@ -25,6 +25,7 @@ const emptySnapshot = {
   customerPaymentMethods: {},
   segments: {},
   discounts: {},
+  discountBulkOperations: {},
   paymentCustomizations: {},
   paymentCustomizationOrder: [],
   deletedPaymentCustomizationIds: {},
